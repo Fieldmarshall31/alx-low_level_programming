3ae354ec9b99cc7d32708edc5f8cf1d46180dfd5
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-11 18:27:19 -0700

    low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(d);
 	printf("Size of a float: %ld byte(s)\n", sizeof(e));
+	return (0);
 }
