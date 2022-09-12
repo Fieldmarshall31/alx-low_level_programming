cc9383032c1a8fc99815823b80fc985bdb22edb1
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-11 18:20:19 -0700

    low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
+* main - Print the size of many var type
 *
-* Description: using the main function
-* this program prints "the size of various types on the computer it is compiled and run on
-* Return: 0
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int; %ld byte(s)\n", sizof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli);
-	printF(Size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(a));
+	printf("Size of an int; %ld byte(s)\n", sizof(b));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d);
+	printf("Size of a float: %ld byte(s)\n", sizeof(e));
 }
