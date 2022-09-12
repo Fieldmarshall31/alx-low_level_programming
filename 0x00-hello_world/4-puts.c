6702bcdf9d8fe2a3384cd79244cc29443b7aa0ae
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-11 17:39:03 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
 * main - print the string in the put function
-* 
+*
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
