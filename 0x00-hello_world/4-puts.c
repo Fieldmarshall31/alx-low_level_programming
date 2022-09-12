08df6dadb0d5a9717f8b4d76ecd2fd8d5c601bcf
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-11 17:14:48 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
- * main - Printing
- *
- * Return
- * ; Always 0 (Successful)
- */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
- }
+
+/**
+* main - print the string in the put function
+* 
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
