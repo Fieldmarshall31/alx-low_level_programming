f2c2d323029d11dac762d0bcb3a551103501a8ab
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-12 18:19:52 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
