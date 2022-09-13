9b0949181dd216f56a2dc844354a552e7f214a2a
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-12 18:34:33 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
