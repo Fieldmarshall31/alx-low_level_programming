691d923c4bb5d027144f60ffe8564e1cef1849f7
Ayoade Emmanuel Ojomo <ojomoayoadeemmanuel@gmail.com>
2022-09-14 05:19:49 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i = o; i <= 9, i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
-	}
 }
